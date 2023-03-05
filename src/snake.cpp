1e9a5766f67640317bd299d9cb05a1c028849350
Nathan Aschenbach <thedenrei@gmail.com>
2023-03-05 00:08:26 -0800

    Update snake.cpp

--- src/snake.cpp
+++ src/snake.cpp
@@ -25,6 +25,7 @@ Adafruit_Protomatter matrix(
         clockPin, latchPin, oePin, true);
 
 #define TRAIL_LENGTH 40
+#define N_SPOTS 4
 #define N_BIKES 4
 typedef int16_t coord_t;
 typedef int16_t color_t;
@@ -46,6 +47,22 @@ struct Bike {
 };
 
 Bike bikes[N_BIKES];
+
+struct Spot {
+    pos_t pos;
+    uint16_t hue;
+    uint8_t radius;
+    uint32_t phase;
+    uint32_t current;
+};
+
+Spot spots[N_SPOTS];
+
+uint32_t prevTime = 0; // Used for frames-per-second throttle
+
+uint32_t secToMicros(uint8_t s) {
+    return s * 1000000L;
+}
 
 pos_t spawnPos() {
     pos_t pos;
@@ -127,7 +144,7 @@ void moveBike(Bike *bike) {
 
 // colorHSV(uint16_t hue, uint8_t sat = 255, uint8_t val = 255)
 
-uint16_t withBrightnessColor565(uint16_t color, double brightness){
+uint16_t withBrightnessColor565(uint16_t color, double brightness) {
     uint8_t r = (color >> 11) & 0x1F;
     uint8_t g = (color >> 5) & 0x3F;
     uint8_t b = color & 0x1F;
@@ -141,16 +158,38 @@ pos_t getTrailIndexPos(Bike *bike, uint8_t trailIndex) {
     return bike->pos[(bike->trailIndex + trailIndex) % TRAIL_LENGTH];
 }
 
+// remap value logarithmically
+uint8_t logmap(uint8_t val) {
+    return 255 * log(val + 1) / log(256);
+}
+
+// remap value exponentially
+uint8_t expmap(uint8_t val) {
+    return 255 * (exp(val / 255.0) - 1) / (exp(1) - 1);
+}
+
+// remap value cubically
+uint32_t remap(uint32_t val, uint32_t p = 3, uint32_t _min = 0, double _max = 255) {
+    return min((_max - _min) * pow(val / (_max - _min), p) + _min, _max);
+}
+
+void drawCircle(int x, int y, int radius, uint16_t color, uint16_t borderColor) {
+    matrix.fillCircle(x, y, radius, color);
+    matrix.drawCircle(x, y, radius, borderColor);
+}
+
 void drawBikes() {
     for (int i = 0; i < N_BIKES; i++) {
         Bike *bike = &bikes[i];
 
         for (int trailIndex = 0; trailIndex < TRAIL_LENGTH; trailIndex++) {
-            uint8_t val = (255 * trailIndex / TRAIL_LENGTH);
+            uint8_t lerp = 255 * trailIndex / TRAIL_LENGTH;
+            uint8_t val = remap(lerp, 3, 16, 160);
             color_t c = matrix.colorHSV(bike->hue, 255, val);
             pos_t pos = getTrailIndexPos(bike, trailIndex);
             matrix.drawPixel(pos.x, pos.y, c);
         }
+
     }
 }
 
@@ -183,7 +222,7 @@ void initBike(Bike *pBike, uint16_t hue) {
 
 uint16_t hueForBikeIndex(int i) {
     return (i * 65535 / N_BIKES); //32768
-}   
+}
 
 // random hue
 uint16_t randomHue() {
@@ -201,6 +240,14 @@ uint16_t randomHueNotTooClose() {
     return hue;
 }
 
+void initSpot(Spot *pSpot) {
+    pSpot->pos = spawnPos();
+    pSpot->hue = randomHue();
+    pSpot->radius = random(1, 4);
+    pSpot->phase = secToMicros(random(1, 4));
+    pSpot->current = 0;
+}
+
 void setup(void) {
     Serial.begin(115200);
     ProtomatterStatus status = matrix.begin();
@@ -210,18 +257,18 @@ void setup(void) {
     for (int i = 0; i < N_BIKES; i++) {
         initBike(&bikes[i], hueForBikeIndex(i));
     }
+    
+    for (int i = 0; i < N_SPOTS; i++) {
+        initSpot(&spots[i]);
+    }
+
+    prevTime = micros();
     Serial.printf("%d total bikes\n", N_BIKES);
 }
 
-bool shouldWait() {
-    static uint32_t prevTime = 0; // Used for frames-per-second throttle
-
-    uint32_t t = micros();
+bool shouldWait(uint32_t t) {
     long delayMicros = 1000000L / MAX_FPS;
-    if (t - prevTime < delayMicros) {
-        return true;
-    }
-    prevTime = t;
+    if (t - prevTime < delayMicros) return true;
     return false;
 }
 
@@ -231,11 +278,24 @@ void bikeDied(Bike *pBike) {
     initBike(pBike, randomHue());
 }
 
+void drawSpot(Spot *pSpot) {
+//    uint8_t val = remap(pSpot->current, 3, 16, 255);
+
+    // fade in and out with a sine wave
+    uint8_t val = 255 * (sin((double)pSpot->current / (double)pSpot->phase * 2 * PI) + 1) / 2;
+
+    color_t c = matrix.colorHSV(pSpot->hue, 255, val);
+    drawCircle(pSpot->pos.x, pSpot->pos.y, pSpot->radius, 0, c);
+}
+
 void loop() {
     // calculations are non-deterministic (don't always take the same amount
     // of time, depending on their current states), this helps ensure that
     // things like gravity appear constant in the simulation.
-    if (shouldWait()) return;
+
+    uint32_t t = micros();
+    if (shouldWait(t)) return;
+    uint32_t dt = t - prevTime;
 
     matrix.fillScreen(0x0);
     for (int i = 0; i < N_BIKES; i++) {
@@ -253,6 +313,17 @@ void loop() {
         }
     }
 
+    for (int i = 0; i < N_SPOTS; i++) {
+        Spot *spot = &spots[i];
+        spot->current += dt;
+        if (spot->current > spot->phase) {
+            spot->current = 0;
+            initSpot(spot);
+        }
+        drawSpot(spot);
+    }
+
     drawBikes();
     matrix.show();
+    prevTime = t;
 }
